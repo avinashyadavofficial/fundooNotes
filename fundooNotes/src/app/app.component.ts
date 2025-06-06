ce1174193cb9226f0ebca5e33eb28774cb9db434
Avinash Yadav <ar5840@srmist.edu.in>
2025-06-06 12:08:39 +0530

    feat: Implement login and registration components with form validation
    - Added ToolbarComponent for navigation with search functionality.
    - Created LoginComponent with reactive form for user authentication.
    - Developed RegistrationComponent with form validation and user registration logic.
    - Integrated AuthGuardService to protect dashboard routes.
    - Implemented HttpService for API calls and UserService for user-related operations.
    - Updated app routing to include login, registration, and dashboard paths.
    - Styled components with CSS for better user experience.
    - Added unit tests for components and services to ensure functionality.

--- fundooNotes/src/app/app.component.ts
+++ fundooNotes/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegistrationComponent } from './Pages/registration/registration.component';
 
-import { LoginComponent } from "./login/login.component";
-import { RegistrationComponent } from "./registration/registration.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, LoginComponent, RegistrationComponent],
+  imports: [CommonModule, LoginComponent, RegistrationComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- fundooNotes/src/app/app.routes.ts
+++ fundooNotes/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegistrationComponent } from './Pages/registration/registration.component';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { AuthGuardService } from './services/authguard/auth-guard.service';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent, pathMatch: 'full' },
+  { path: 'register', component: RegistrationComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuardService]
+  }
+];
